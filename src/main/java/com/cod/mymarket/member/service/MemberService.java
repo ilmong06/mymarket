53b7619512b6eae8b619bcb9609538ed48bd1387
ilmong06 <ilmongcoding@gmail.com>
2024-07-18 18:44:01 +0900

    소셜로그인 성공

--- src/main/java/com/cod/mymarket/member/service/MemberService.java
+++ src/main/java/com/cod/mymarket/member/service/MemberService.java
@@ -35,38 +35,5 @@ public class MemberService {
     }
 
 
-    @Transactional
-    public boolean isFirstSocialLogin() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof OAuth2User) {
-            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-            String nickname = oAuth2User.getAttribute("nickname");
-            Optional<Member> existingMember = memberRepository.findByUsername(nickname);
-            return existingMember.isEmpty(); // 존재하지 않는 경우에만 true 반환
-        }
-        return false;
-    }
-    @Transactional
-    public void socialjoin(String address) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof OAuth2User) {
-            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-            String username = oAuth2User.getAttribute("name");
-            String email = oAuth2User.getAttribute("email");
-            String nickname = oAuth2User.getAttribute("nickname");
 
-            // 주소를 이용하여 회원가입 처리
-            Member member = Member.builder()
-                    .username(username)
-                    .email(email)
-                    .nickname(nickname)
-                    .address(address)
-                    .coupon(null) // 쿠폰 정보 초기화
-                    .build();
-
-            memberRepository.save(member);
-        } else {
-            throw new IllegalStateException("OAuth2User not found in SecurityContext");
-        }
-    }
 }
